bc4beaafa48de5b762b640263dcddb3568fe939d
zenta514 <131842396+zenta514@users.noreply.github.com>
2023-11-22 14:28:21 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript code for Tetris game logic
+
+// Define Tetris shapes, their rotations, movements, etc.
+
+// Create functions to handle user input (keyboard controls)
+
+// Implement game board logic (collision detection, clearing lines, scoring)
+
+// Handle game loop (block falling, updating the board, checking for game over)
+
+// Initialize the game and start the game loop
